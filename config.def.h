b9289d1818010b577e4d11a5cd315ec611777d51
Tomas Divis <tomas.divis@nic.cz>
2016-11-02 17:53:30 +0100

    Audacious play/payse button

--- config.def.h
+++ config.def.h
@@ -78,12 +78,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", "tmux", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
+static const char *playpausecmd[]  = { "audacious", "--play-pause", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ ControlMask|Mod1Mask,         XK_l,      spawn,          {.v = lockcmd } },
+	{ NULL,                         0x1008ff14, spawn,         {.v = playpausecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
         //	{ MODKEY,                       XK_w,      tabmode,        {-1} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
