d4296c5f17ee7c286ae7a7e74853d8f07eee9ff4
Tomas Divis <tomas.divis@nic.cz>
2020-01-29 10:56:27 +0100

    Add dmenu_extended.

--- config.def.h
+++ config.def.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_extended_run", "--no-settings", NULL };
 static const char *termcmd[]  = { "st", "tmux", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 static const char *sleepcmd[]  = { "sudo", "pm-suspend", NULL };
